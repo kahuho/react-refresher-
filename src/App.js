05cb8aac8d05fb5dd1276cd306673282ee2f6fdb
kahuho <kahuhobrian7@gmail.com>
2022-09-19 13:36:07 +0300

    JSx, components, and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const partsArray = [];
+  const parts = [
+    {part: 'Fundamentals of React', exercise: 10, id:1},
+    {part: 'Using props to pass data', exercise: 7, id:2},
+    {part: 'State of a Component', exercise:14, id:3},
+  ];
+  parts.filter((item) => item.exercise ? partsArray.push(item.exercise): console.log('nope'));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        justifyContent: "center"
+      }}
+    >
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total partsArray={partsArray} />
     </div>
   );
-}
+};
 
 export default App;
+
+
